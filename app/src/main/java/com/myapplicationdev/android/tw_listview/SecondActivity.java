ad09ce1ba9d983a73cc2598f22b25398eafe6a12
19023702 <19023702@myrp.edu.sg>
2021-04-20 14:27:03 +0800

    Done

--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,19 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
+    ArrayList<Module> module;
+    ArrayAdapter<Module> aa;
+
 
 
     @Override
@@ -23,6 +29,27 @@ public class SecondActivity extends AppCompatActivity {
         Intent i = getIntent();
         String year = i.getStringExtra("year");
         tvYear.setText(year);
+
+        module = new ArrayList<Module>();
+
+        if (year.equals("Year 1")) {
+            module.add(new Module("C105", R.drawable.prog));
+            module.add(new Module("C108", R.drawable.prog));
+            module.add(new Module("G952", R.drawable.nonprog));
+        } else if (year.equals("Year 2")) {
+            module.add(new Module("C308", R.drawable.prog));
+            module.add(new Module("C346", R.drawable.prog));
+            module.add(new Module("G953", R.drawable.nonprog));
+        } else {
+            module.add(new Module("C349", R.drawable.prog));
+            module.add(new Module("C347", R.drawable.prog));
+            module.add(new Module("C390", R.drawable.nonprog));
+        }
+
+        lv = findViewById(R.id.lvModule);
+
+        aa = new ModuleAdapter(this, R.layout.row, module);
+        lv.setAdapter(aa);
 
     }
 
